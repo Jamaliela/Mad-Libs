29a4421ecda7571278c789b1785f8fe18ac8cf55
alfarozavalae <Newgirl1>
2018-10-22 23:24:27 -0400

    finished coding and readme. comments have been added

--- t09_stubs.py
+++ t09_stubs.py
@@ -7,44 +7,36 @@
 # Gain practice using strings.
 #################################################################################
 # Acknowledgements:
-#
-#
 #################################################################################
 
 
 def story_time(answers):
     """
-    Docstring for function_1
+        This function alocates the user's input into the text to print an mad lib email.
+    :param answers: parameter to get the user's input from main
+    :return:
     """
-    print('''Dear professor, {0}
+    print('''Dear professor, {0}  
     
-    I wanted to let you know that I will not be able to attend class today because I broke my {1}.
-    When I was {2} on the sofa. I feel very {3} because I will miss your lecture.
+    I wanted to let you know that I will not be able to attend class today because I broke my {1} when I was {2} on the sofa. 
+    I feel very {3} to miss your lecture.
     Also, last week, I did not submit the homework in Github because my {4} ate the paper I was working the algorithm on.
     
     "I apologize for being such a {5} student.
-    
     Best,
-    {6}'''.format(*answers))
-
-
-def function_2():
-    """
-    Docstring for function_2
-    """
-    pass
-    # ...
+    {6}'''.format(*answers))                    # this print statement contains a long string that uses {} to replace the numbers with
+                                                # the input's that the user entered in main. we use format to add them in the spaces
 
 
 def main():
     """
-    Docstring for main
+    Main function to ask the user for seven inputs that will be used in a function. uses for loop to run the same input statement and append them afterwards.
     """
-    input_user = ["last_name", "body_part", "ing_verb", "adjective", "animal", "adjective_2", "your_name"]
-    answers = []
-    for i in range(7):
-        answers.append((str(input("Please enter " + input_user[i]))))
-    story_time(answers)
+    input_user = ["last_name", "body_part", "ing_verb", "adjective", "animal", "adjective_2", "your_name"]          # seven inputs that we will ask the user for
+    answers = []                        # creating empty list
+    for i in range(7):                  # loop to run 7 times and ask user for input
+        answers.append((str(input("Please enter " + input_user[i]))))       # appending the user's input in a list
+    story_time(answers)                                                     # calling story_time function
 
 
 main()
